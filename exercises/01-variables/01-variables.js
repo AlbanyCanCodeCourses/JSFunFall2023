9d657a6d6e5cd1f3590f1b6b8d003a2732dcacd2
Edwin <Edwinallen94@aol.com>
2023-10-04 18:52:28 -0400

    Finished exercises 1 and 2

--- exercises/01-variables/01-variables.js
+++ exercises/01-variables/01-variables.js
@@ -4,3 +4,9 @@
  */
 
 // WRITE YOUR ANSWER BELOW THIS LINE
+
+let myNumber = 4; //my number should be 4
+
+console.log(myNumber + 3); // it should be 4 + 7
+
+console.log(`my number is ${myNumber}`); // a sentence with my favorite number 
