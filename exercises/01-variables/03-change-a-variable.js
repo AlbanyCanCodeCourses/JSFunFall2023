2cae446edc5d92f9beee01fd74290a5b8aaf7473
avrubio <ariadna.vanegas@gmail.com>
2023-10-04 20:40:23 -0400

    01. Variables and 02. Numbers completed

--- exercises/01-variables/03-change-a-variable.js
+++ exercises/01-variables/03-change-a-variable.js
@@ -4,5 +4,5 @@ let price = 5.99;
  * Hurray inflation. Without modifying the first line,
  * change the variable "price" so that it is a value that is greater than 5.99.
  */
-
 // WRITE YOUR ANSWER BELOW THIS LINE
+price = 12.99;
