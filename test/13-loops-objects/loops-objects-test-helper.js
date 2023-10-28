a397147d62ae85b8c47453ef0d1ecfc29a59c1f2
matinaspatsos <matinaspatsos@gmail.com>
2023-10-28 17:18:41 -0400

    Add return-object-values exercise

--- test/13-loops-objects/loops-objects-test-helper.js
+++ test/13-loops-objects/loops-objects-test-helper.js
@@ -4,7 +4,16 @@ const generateLinksStr = getAnswer(
   "../exercises/13-loops-objects/01-loop-through-object.js"
 );
 
+const returnObjectValuesStr = getAnswer(
+  "../exercises/13-loops-objects/03-return-object-values.js"
+);
+
 export const generateLinks = eval(`(links) => {
   ${generateLinksStr}
   return html;
+}`);
+
+export const returnObjectValues = eval(`(blogPost) => {
+  ${returnObjectValuesStr}
+  return values;
 }`);
