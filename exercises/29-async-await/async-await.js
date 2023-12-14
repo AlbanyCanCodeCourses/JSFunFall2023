a7a6629278dc4e4727844683001aa646b13f7478
avrubio <ariadna.vanegas@gmail.com>
2023-12-13 19:12:24 -0500

    29 async and await problem solved

--- exercises/29-async-await/async-await.js
+++ exercises/29-async-await/async-await.js
@@ -8,4 +8,52 @@
    * This is the API you will be using. The method will be GET.
    * https://ron-swanson-quotes.herokuapp.com/v2/quotes
    */
+  //This is telling the function that this is an async function
+  const myQuotes = async () => {
+    try {
+      //until this request is completed the next steps can NOT occur
+      let quote = await axios(
+        "https://ron-swanson-quotes.herokuapp.com/v2/quotes"
+      );
+      //once request goes thru the data is saved in this variable
+      let data = quote.data[0];
+
+      let quoteBtn = document.querySelector(".btn");
+      let blockQuote = document.querySelector("#quote");
+      //listens to the button being clicked to then display the quote
+      quoteBtn.addEventListener("click", () => {
+        //changes the test in this blockquote element
+        blockQuote.textContent = data;
+      });
+    } catch (err) {
+      //if it goes wrong and the api isn't working an error should appear
+      return console.log("error", err);
+    }
+  };
+  myQuotes();
+})();
+// first original way that I came back to test
+(async () => {
+  /**
+   * As a user, I should be able to click on a button and get a quote, so I can learn about what ron-swanson's opinions are.
+   *
+   *  Developer notes:
+   *  In the HTML, there is an element that you can use to populate the quotes(#blockquote) or feel free to use your own design.
+   *
+   * This is the API you will be using. The method will be GET.
+   * https://ron-swanson-quotes.herokuapp.com/v2/quotes
+   */
+  try {
+    let quote = await axios(
+      "https://ron-swanson-quotes.herokuapp.com/v2/quotes"
+    );
+    let data = quote.data[0];
+    let quoteBtn = document.querySelector(".btn");
+    let blockQuote = document.querySelector("#quote");
+    quoteBtn.addEventListener("click", () => {
+      blockQuote.textContent = data;
+    });
+  } catch (err) {
+    return console.log("error", err);
+  }
 })();
